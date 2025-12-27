370f4bf9a571575089f7cf7e0a8fdca254fb2554
Groman-S <goyanov99@mail.ru>
2025-12-27 11:35:39 +0300

    Исправил ошибку отрицательной частоты обновления таба. Избавился от лишних таймеров таба.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.goyanov"
-version = "1.4.0"
+version = "1.4.1"
 
 repositories {
     maven("https://hub.spigotmc.org/nexus/content/groups/public/")
--- src/main/kotlin/com/goyanov/essentials/tab/TabUpdateTimer.kt
+++ src/main/kotlin/com/goyanov/essentials/tab/TabUpdateTimer.kt
@@ -23,22 +23,22 @@ class TabUpdateTimer private constructor(var papiEnabled: Boolean) : BukkitRunna
     private var footerUpdateInterval = EssentialsRGX.inst().config.getInt("tab.update-ticks.footer")
     private var playerNamesUpdateInterval = EssentialsRGX.inst().config.getInt("tab.update-ticks.player-names")
 
-    private var headerUpdateTick = 0
-    private var footerUpdateTick = 0
-    private var playerNameUpdateTick = 0
+    private var currentTick = 0
 
     override fun run() {
 
-        if (headerUpdateTick++ % headerUpdateInterval == 0) {
+        if (headerUpdateInterval >= 0 && currentTick % headerUpdateInterval == 0) {
             Bukkit.getOnlinePlayers().forEach { player -> sendCustomHeaders(player, papiEnabled) }
         }
 
-        if (footerUpdateTick++ % footerUpdateInterval == 0) {
+        if (footerUpdateInterval >= 0 && currentTick % footerUpdateInterval == 0) {
             Bukkit.getOnlinePlayers().forEach { player -> sendCustomFooters(player, papiEnabled) }
         }
 
-        if (playerNameUpdateTick++ % playerNamesUpdateInterval == 0) {
+        if (playerNamesUpdateInterval >= 0 && currentTick % playerNamesUpdateInterval == 0) {
             Bukkit.getOnlinePlayers().forEach { player -> updatePlayerTabName(player, papiEnabled) }
         }
+
+        currentTick++
     }
 }
